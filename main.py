6b60683ec11ab08047dc68263a80b6ce241936c1
workdl <workdl@local.loc>
2021-03-02 16:43:52 +0300

    test sort

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from socket import gethostbyname
 
 def import_file():
     listMT = {}
-    with open('Addresses.CDB', 'r', encoding="cp866") as fileAddress:
+    with open('Addresses.cdb', 'r', encoding="cp866") as fileAddress:
         # print(fileAddress.read())
         all_info_mt_ip_clear = ''
         file = fileAddress.read().split('M2')
@@ -29,31 +29,44 @@ def import_file():
             all_info_mt_ip_clear = ''
     return listMT
 
-def host_availability(host):
-    try:
-        if gethostbyname(host) == host:
-            return '[ok]'
-    except IOError:
-        return '[host not available]'
-    except:
-        return '[host not available]'
+
+def host_availability(dict_filter):
+    pass
+
 
 def check_settings(host, command):
     port = 22
-    user = 'support'
-    password = 'support'
+    user = 'admin'
+    password = ''
 
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname=host, username=user, password=password, port=port)
-    shell = client.invoke_shell()
-    shell.send('queue type edit pcq-download-default pcq-rate \x01\0B 5m \x01\0f')
-    #shell.send('\x01\0B 5m \x01\0f')
-
-
-
-
+    stdin, stdout, stderr = client.exec_command(command)
+    data = stdout.read() + stderr.read()
     client.close()
-    #return data
 
-print(check_settings('192.168.50.1', 'queue type edit pcq-download-default pcq-rate \n 25m \n'))
+    return data
+
+
+def sort_import_file(import_dict, key):
+    if key != '*':
+        for date_key in import_dict:
+            if date_key == key:
+                dict_fit = {date_key: import_dict[date_key]}
+            else:
+                dict_no_fit = {date_key: import_dict[date_key]}  # словарь не подходящих по фильтру
+        return dict_fit
+    else:
+        return import_dict
+
+
+
+
+print(import_file(), '\n ---------------')  # Импорт и парсинг файла
+import_f = import_file()
+
+print(sort_import_file(import_f, '*'), '\n ---------------')  # сортировка по ключу
+sort_import_f = sort_import_file(import_f, '*')
+
+print(host_availability(sort_import_f))  # доступность хоста
