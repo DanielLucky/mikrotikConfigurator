96c6f12b365194c7ce9116b733ed0b1844c8d2e2
DanielLucky <62309571+DanielLucky@users.noreply.github.com>
2021-03-03 23:20:23 +0300

    исправлен баг с отчисткой списка полученых значений

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ DEVNULL = subprocess.DEVNULL
 
 def decorator(import_list_mt):
     print(f'Выберите действие:\n'
-          f'Сортировка - [1] ::: '
-          f'Доступность хостов - [2] ::: '
-          f'Проверка настроек - [3] ::: '
-          f'Применение настроек - [4] ::: '
-          f'Импорт файла - [5]')
+          f'[1] Сортировка  ::: '
+          f'[2] Доступность хостов  ::: '
+          f'[3] Проверка настроек  ::: '
+          f'[4] Применение настроек ::: '
+          f'[5] Импорт дефолтного файла')
     select = input()
     if select == '1':
         print('Sort key:', end='')
         sort_import_f = sort_import_file(import_list_mt, input())
         pprint.pprint(sort_import_f)
         print('Выбрано:', len(sort_import_f), end='\n\n')
+
         return sort_import_f
 
     elif select == '2':
@@ -30,6 +31,7 @@ def decorator(import_list_mt):
         if len(import_list_mt.keys() - host_av.keys()) != 0:
             pprint.pprint(import_list_mt.keys() - host_av.keys())
         print('Выбрано:', len(host_av), end='\n\n')
+
         return host_av
 
     elif select == '3':
@@ -37,6 +39,7 @@ def decorator(import_list_mt):
         set_check = check_settings(import_list_mt, input())  # 'put [queue type get [find name=pcq-download-default] pcq-limit]' pcq_download
         pprint.pprint(set_check)
         print('Выбрано:', len(set_check), end='\n\n')
+
         return set_check
 
     elif select == '4':
@@ -46,13 +49,14 @@ def decorator(import_list_mt):
         import_list_default = import_file()  # Импорт и парсинг файла
         pprint.pprint(import_list_default)
         print('Выбрано:', len(import_list_default))
+
         return import_list_default
 
 
 def import_file():
     listMT = {}
     print('Импортирование файла Addresses.CDB')
-    with open('Addresses.CDB', 'r', encoding="cp866") as fileAddress:
+    with open('Addresses.cdb', 'r', encoding="cp866") as fileAddress:
         # print(fileAddress.read())
         all_info_mt_ip_clear = ''
         file = fileAddress.read().split('M2')
@@ -79,16 +83,15 @@ def import_file():
 
 
 def host_availability(dict_filter):
-    availability_dict = {}
     for data in dict_filter:
         # print(data)
         for data_ip in dict_filter[data]:
             # print(data_ip)
             requesrt = subprocess.call(["ping", "-c", "1", dict_filter[data]['ip']], stdout=DEVNULL)
             if requesrt == 0:
-                availability_dict[data] = {'ip': dict_filter[data]['ip']}
-            # print(availability_dict)
-    return availability_dict
+                dict_filter[data]['ip'] = dict_filter[data]['ip']
+
+    return dict_filter
 
 
 def check_settings(host_dict, command):
@@ -102,10 +105,8 @@ def check_settings(host_dict, command):
 
         for data_ip in host_dict[data]:
 
-            #print(data_ip)
             client = paramiko.SSHClient()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            #print('mt', data, host_dict[data]['ip'], 'chenge')
             try:
                 client.connect(hostname=host_dict[data]['ip'], username=user, password=password, port=port, banner_timeout=200, look_for_keys=False)
             except:
@@ -113,14 +114,9 @@ def check_settings(host_dict, command):
             stdin, stdout, stderr = client.exec_command(command)
             data_out = stdout.read() + stderr.read()
             client.close()
-
-
-            #data_out_dict[data] = {'ip': host_dict[data]['ip'], command: data_out.decode('utf-8')}
-            data_out = data_out.decode('utf-8')
+            data_out = data_out.decode('utf-8')[:-2]
 
         host_dict[data][command] = data_out
-        print('Заполнение:', host_dict[data][command], data_out)
-        #command = ''
 
     return host_dict
 
@@ -140,10 +136,10 @@ def sort_import_file(import_dict, key):
         for data in import_dict:  # поиск при точном значении
             if data == key[0]:
                 sort_dict[data] = import_dict[data]
+
     return sort_dict
 
 
-print('Импортирование файла Addresses.CDB')
 import_f = import_file()  # Импорт и парсинг файла
 pprint.pprint(import_f)
 print('Выбрано:', len(import_f))
